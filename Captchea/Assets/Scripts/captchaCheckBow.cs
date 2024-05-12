0a50cac7e5ffd66a4032e9d10f4fd94c0f7bf9d3
quader-arian <arianq@my.yorku.ca>
2024-05-12 13:44:51 -0400

    completed the 2 pic lvl

---
+++ Captchea/Assets/Scripts/captchaCheckBow.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class captchaCheckBow: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject nextLevel;
+    public GameObject currentLevel;
+    public GameObject gameOver;
+    public GameObject textBoxObject;
+    public GameObject loading;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            OnMouseDown();
+        }
+    }
+
+
+    public void OnMouseDown()
+    {
+        string text = textBoxObject.GetComponent<TMP_InputField>().text;
+        if(text == "bow" || text == "BOW")
+        {
+            StartCoroutine(next());
+            
+        }
+        else if(text != "")
+        {
+            StartCoroutine(loss());
+        }
+    }
+
+    IEnumerator next()
+    {
+        yield return new WaitForSeconds(1);
+        currentLevel.SetActive(false);
+        loading.GetComponent<loadingText>().level = nextLevel;
+        loading.SetActive(true);
+    }
+    IEnumerator loss()
+    {
+        yield return new WaitForSeconds(1);
+        currentLevel.SetActive(false);
+        gameOver.SetActive(true);
+    }
+}
